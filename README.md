5274c8826124b63ba5874038ccf23df533bd78b2
Icenowy Zheng <icenowy@outlook.com>
2015-10-07 23:04:51 +0800

    initial commit, which is now used on the Tablet.

--- README.md
+++
@@ -1,2 +0,0 @@
-# linux-kernel-lichee-a33
-A purified version of Lichee kernel of Allwinner A33 SoC
---
+++ drivers/input/touchscreen/gt9xxf/diff_net.sh
@@ -0,0 +1,1 @@
+wget https://raw.githubusercontent.com/allwinner-zh/linux-3.4-sunxi/master/drivers/input/touchscreen/gt9xxf/$1 -O - | diff - $1
